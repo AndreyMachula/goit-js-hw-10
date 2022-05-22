7725c17941c9a3f69745fd73d3b68810eaced0b3
AndreyMachula <amcz2020@gmail.com>
2022-05-22 16:03:52 +0200

    Create api-service.js

---
+++ src/js/api-service.js
@@ -0,0 +1,14 @@
+const BASE_URL = 'https://restcountries.com/v3.1';
+
+function fetchCountry(name) {
+
+  return fetch(`${BASE_URL}/name/${name}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    };
+
+    return response.json();
+  });
+}
+
+export default { fetchCountry };
